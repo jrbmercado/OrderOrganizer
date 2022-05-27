c8091de55d4c2dbf1b223c7f96eae12c90f9eb75
Justin Mercado <justinreianmercado@gmail.com>
2022-05-26 19:36:08 -0700

    Added Interface and Person class - Need to complete UML diagram prior to continuing development. Unclear how classes will interact with each other right now.

--- src/Organizer/Interface.java
+++ src/Organizer/Interface.java
@@ -1,10 +1,25 @@
 package Organizer;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Interface {
-
+	
+	// HashList of Persons
+	HashSet<Person> party = new HashSet();
+	
 	public static void main(String[] args) {
-		System.out.println("Wow!");
-
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.print("How many people total? ");
+		int numPeople = myScanner.nextInt();
+		
+		System.out.println("There are " + numPeople+ " members in your party");
+		
+		// Fill the HashSet with Names
+		
+		
+		
 	}
 
 }
